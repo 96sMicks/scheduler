08bdfcf87002b5e22e68a7842a7da25c4a111331
Melkias Abraham <melkiasae@hotmail.com>
2021-09-28 16:38:30 -0400

    DayListItem stories for full, clickable, unselected & selected implemented

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots}</h3>
     </li>
   );
 }
