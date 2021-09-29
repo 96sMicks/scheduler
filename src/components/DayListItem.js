354c22df5c7ffbb8d3c9fdbf6529242fbc9a4402
Melkias Abraham <melkiasae@hotmail.com>
2021-09-28 21:05:32 -0400

    DayListItem testing scripts & is now able to format the spots counter

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,19 +2,31 @@ import React from "react";
 
 import "components/DayListItem.scss"
 import classNames from "classnames";
+import { tSExportAssignment } from "@babel/types";
+
 
 export default function DayListItem(props) {
+
+  function formatSpots(dynamicNumber) {
+
+    if (dynamicNumber === 0 ){
+      return "no spots remaining"
+    } else if ( dynamicNumber === 1) {
+      return "1 spot remaining"
+    }
+    return `${dynamicNumber} spots remaining`
+  }
+  
 
   let dayClass = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : props.spots === 0
   });
   
-  console.log(dayClass)
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2>{props.name}</h2>
-      <h3>{props.spots}</h3>
+      <h3>{ formatSpots(props.spots) }</h3>
     </li>
   );
 }
