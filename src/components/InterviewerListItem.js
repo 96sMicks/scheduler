cba083f1ef58dc6cc8e5adbd274352f8bd54fb57
Melkias Abraham <melkiasae@hotmail.com>
2021-09-30 19:51:30 -0400

    Interview List now able to show names when selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,32 +1,21 @@
-import React, { useState } from "react";
-
+import React from "react";
 import "components/InterviewerListItem.scss"
 import classNames from "classnames";
 
 
 export default function InterviewerListItem(props) {
-  const [interviewer, setInterviewer] = useState("")
-
   let interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected" : props.selected
   });
-
-  let imgClass = classNames("interviewers__item-image", {
-    "interviewers__item-image--selected" : props.selected
-  });
-
-  console.log(props)
-  console.log(interviewerClass)
-  console.log(imgClass)
-
+  
   return (
     <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
   <img
-    className={imgClass}
+    className="interviewers__item-image"
     src={props.avatar}
     alt={props.name}
   />
-  {props.name}
+  {props.selected && props.name}
 </li>
-  )
+  );
 }
