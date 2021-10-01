476b0f62440a92eab775319c1480f47cebe30811
Melkias Abraham <melkiasae@hotmail.com>
2021-09-30 20:42:33 -0400

    Removed the hardcoded data from the top of the file and used the one from props in App.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,27 +1,10 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function DayList(props) {
   
-  const parsedDayList = days.map(day => 
+  const parsedDayList = props.days.map(day => 
   <DayListItem 
     key={day.id}
     name={day.name} 
