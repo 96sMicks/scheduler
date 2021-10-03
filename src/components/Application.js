ed909185663f8d31adfca42a6703cb45a23684e9
Melkias Abraham <melkiasae@hotmail.com>
2021-10-03 18:22:45 -0400

    Added appointments to state instead of hardcode

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,54 +6,65 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-    // interview: {
-    //   student: "Mickey",
-    //   interviewer: {
-    //     id: 2,
-    //     name: "Damian Lillard",
-    //     avatar: "https://i.imgur.com/twYrpay.jpg",
-    //   },
-    // },
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Jayson Tatum",
-      interviewer: {
-        id: 2,
-        name: "Scott Smith",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     // interview: {
+//     //   student: "Mickey",
+//     //   interviewer: {
+//     //     id: 2,
+//     //     name: "Damian Lillard",
+//     //     avatar: "https://i.imgur.com/twYrpay.jpg",
+//     //   },
+//     // },
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Jayson Tatum",
+//       interviewer: {
+//         id: 2,
+//         name: "Scott Smith",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       },
+//     },
+//   },
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([])
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([])
 
-  const parsedAppointmentList = appointments.map(app =>
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+
+  const setDay = day => setState({...state, day});
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+
+  const parsedAppointmentList = dailyAppointments.map(app =>
     <Appointment
       key={app.id}
       {...app}
@@ -75,8 +86,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
             // setDay={day => console.log(day)} was used to see the click day on console
           />
