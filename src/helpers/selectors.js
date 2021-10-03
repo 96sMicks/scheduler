089f01a6303c6b1a54ba11aa1b3552ba73c0d143
Melkias Abraham <melkiasae@hotmail.com>
2021-10-03 15:23:45 -0400

    Now able to access the appointment arrays within a selected day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,11 @@
 export function getAppointmentsForDay(state, day) {
   const dayArray = state.days
-  const dayCheck = dayArray.filter(x => x.name === day )
-  
-  console.log(dayCheck)
-  return dayCheck
+  const daysFilter = dayArray.filter(selectedDay => selectedDay.name === day)
+
+  // An ARRAY
+  // console.log(daysFilter) 
+
+  for (const day of daysFilter) {
+    return day.appointments
+  }
 }
