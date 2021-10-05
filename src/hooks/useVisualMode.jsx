759e6dcc3cb6fb696113e8145720a5397488be94
Melkias Abraham <melkiasae@hotmail.com>
2021-10-05 00:51:24 -0400

    Back function now functional and passes all test, a back limit was added so that we don't go pass the intial mode set

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -1,18 +1,43 @@
-import { useState  } from "react";
+import { useState } from "react";
 
 const useVisualMode = (initialMode) => {
-const [mode, setMode] = useState(initialMode);
-const [history, setHistory] = useState(initialMode);
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
-const transition = (newMode) => {
-  setMode(newMode);
-}
+  const transition = (newMode) => {
+    // Updates the current mode state to a NEW MODE value
+    setMode(newMode);
 
-const back = () => {
-  
-}
+    // keep track of the history of the modes but do NOT mutate state,
+    setHistory([...history, newMode]);
 
-return {  mode, transition, back };
-}
+    // returns an array with a list of all the modes
+    const updatedHistoryArray = [...history];
+
+    // pushes the new mode into the history array
+    updatedHistoryArray.push(newMode);
+
+    // console.log(mode)
+    // console.log("xxxxxxxxxx", parsedHistoryArray)
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      // so we avoid mutating state, make a copy
+      const prevArray = [...history];
+
+      prevArray.pop();
+
+      // console.log(history)
+      // console.log(prevArray)
+
+      setHistory(prevArray);
+
+      setMode(prevArray[prevArray.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
+};
 
 export default useVisualMode;
