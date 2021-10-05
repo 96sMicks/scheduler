6a6a6e2138122925681be270d6406e08583eb2b0
Melkias Abraham <melkiasae@hotmail.com>
2021-10-04 21:46:06 -0400

    Transition function to change Mode is now added to the return object in useVisualMode

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -3,7 +3,11 @@ import { useState  } from "react";
 const useVisualMode = (initialMode) => {
 const [mode, setMode] = useState(initialMode)
 
-return { mode };
+const transition = (newMode) => {
+  setMode(newMode);
+}
+
+return {  mode, transition };
 }
 
 export default useVisualMode;
