36a1053782055d6098eacbb1c01f67b997d32958
Melkias Abraham <melkiasae@hotmail.com>
2021-10-04 20:08:00 -0400

    Interview selector now passes all tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,10 @@
 export function getAppointmentsForDay(state, day) {
-  const dayArray = state.days
-  const daysFilter = dayArray.filter(selectedDay => selectedDay.name === day)
+  const dayArray = state.days;
+  const daysFilter = dayArray.filter((selectedDay) => selectedDay.name === day);
 
   const filteredDayAppointments = [];
 
-  if (dayArray.length === 0){
+  if (dayArray.length === 0) {
     return [];
   }
 
@@ -13,20 +13,28 @@ export function getAppointmentsForDay(state, day) {
   }
 
   for (const day of daysFilter) {
-    const appointmentSlots = day.appointments
+    const appointmentSlots = day.appointments;
 
-    for(const id of appointmentSlots) {
-
-      if(state.appointments[id]){
-        filteredDayAppointments.push(state.appointments[id])
+    for (const id of appointmentSlots) {
+      if (state.appointments[id]) {
+        filteredDayAppointments.push(state.appointments[id]);
       }
-
     }
-    return filteredDayAppointments
+    return filteredDayAppointments;
   }
-};
+}
 
 export function getInterview(state, interview) {
-  console.log("xxxx", interview.interviewer)
-  console.log("yyyyy", state.interviewers)
+  if (interview === null) {
+    return null;
+  }
+
+  const interviewerIDNumber = interview.interviewer;
+  const interviewersObject = state.interviewers;
+
+  if (interviewersObject[interviewerIDNumber]) {
+    const formattedInterviewer = interviewersObject[interviewerIDNumber];
+    interview.interviewer = formattedInterviewer;
+  }
+  return interview;
 }
