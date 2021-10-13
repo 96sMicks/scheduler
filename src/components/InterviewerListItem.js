1bd2ca9a4cbffb22551a6243e30264058ef34701
Melkias Abraham <melkiasae@hotmail.com>
2021-10-13 12:22:06 -0400

    got ride of props destrucutre to fall in line with the rest of app files

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,20 +4,19 @@ import classNames from "classnames";
 
 
 export default function InterviewerListItem(props) {
-  const{ name, avatar, selected, setInterviewer } = props
 
   let interviewerClass = classNames("interviewers__item", {
-    "interviewers__item--selected" : selected
+    "interviewers__item--selected" : props.selected
   });
   
   return (
-    <li className={interviewerClass} onClick={setInterviewer}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
   <img
     className="interviewers__item-image"
-    src={avatar}
-    alt={name}
+    src={props.avatar}
+    alt={props.name}
   />
-  {selected && name}
+  {props.selected && props.name}
 </li>
   );
 }
