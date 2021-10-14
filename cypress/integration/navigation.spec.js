54f93f8e4f42484026180034ca6da3cc762b0630
Melkias Abraham <melkiasae@hotmail.com>
2021-10-14 03:59:49 -0400

    E2E test for the booking completed

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,4 +1,5 @@
 describe("Navigation", () => {
+  
   it("should visit root", () => {
     cy.visit("/");
   });
@@ -6,8 +7,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/")
-    cy.get("li").contains("Tuesday").click()
+  
     cy.contains("li", "Tuesday")
-      .should("have.css", "background-color", "rgb(242, 242, 242)")
+      .click()
+      .should("have.class", "day-list__item--selected")
   });
 });
